7cea5ab1bac83a0e1b8a413acbea9bdb0edf4335
tsubasa111 <>
2022-02-21 13:42:45 +0900

    微修正と新しいファイルを作成

--- laravel/app/Http/Controllers/Controller.php
+++ laravel/app/Http/Controllers/Controller.php
@@ -13,18 +13,4 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
-
-    /**
-     * プレフィックスを削除
-     */
-    public function RemovePrefixFormating(array $values, string $prefix): array
-    {
-        $formated = [];
-        foreach ($values as $key => $value) {
-            $key = str_replace($prefix, '', $key);
-            $formated[$key] = $value;
-        }
-
-        return $formated;
-    }
 }
